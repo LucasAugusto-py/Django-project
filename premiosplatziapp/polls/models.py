4e136262458a468f80bf21408d25c77401e6c742
Lucas Silva <lucasaugustosilvagomez@gmail.com>
2022-08-03 17:37:31 -0300

    Commit 5: migrations created successfuly

--- premiosplatziapp/polls/models.py
+++ premiosplatziapp/polls/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=300)
+    pub_date = models.DateTimeField("date published")
+
+class Choise(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choise_text = models.CharField(max_length=300)
+    votes = models.IntegerField(default=0)
