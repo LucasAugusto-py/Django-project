c36591b180aa4da1682a8c4fe8f3d0e14d21507e
Lucas Silva <lucasaugustosilvagomez@gmail.com>
2022-08-04 17:55:53 -0300

    Commit 10: class 26 ended, view detail ended

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import Question, Choice
 
 # Create your views here.
 def index(request):
@@ -11,11 +12,22 @@ def index(request):
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk = question_id)
-    return render(request, "polls/details.html",{
+    return render(request, "polls/detail.html",{
         "question": question
     })
 def results(request, question_id):
     return HttpResponse(f"Estas viendo los resutlados de la pregunta numero {question_id}")
 
 def vote(request, question_id):
-    return HttpResponse(f"Estas votando a la pregunta numero {question_id}")
+    question = get_object_or_404(Question, pk = question_id)
+    try:
+        selected_choice = question.choice_set.get(pk = request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "polls/details.html",{
+            "polls/details.html": question,
+            "error_message": "No elegiste una respuesta"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id)))
